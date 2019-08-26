beed6f61f08d7403fbbf0e818c02e17dc9b150bf
Hau Nguyen <haunttpd00207@gmail.com>
2019-08-26 11:03:35 +0700

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -35,7 +35,7 @@ class User < ApplicationRecord
     def authenticated?(attribute, token)
       digest = send("#{attribute}_digest")
       return false if digest.nil?
-      BCrypt::Password.new(digest).is_password?(token)
+      BCrypt::Password.new(digest).is_password?(token) # return False if in Database have the ****_digest tuong ung voi ID user
     end
 
     #forget the user
@@ -44,23 +44,45 @@ class User < ApplicationRecord
     end
 
     def activate
-      update_columns(activated: true, activated_at: Time.zone.now)
+      #update on database
+
+      update_columns(activated: true, activated_at: Time.zone.now)  #thay the update_attribute methods
+      
     end
 
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
-      private
-    # Converts email to all lower-case.
-    def downcase_email
-      self.email = email.downcase
+
+
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+      
     end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end  
+    
+      def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+      end
+
+
+
+
+      private
+    # Converts email to all lower-case.
+        def downcase_email
+          self.email = email.downcase
+        end
+
+        # Creates and assigns the activation token and digest.
+        def create_activation_digest
+          self.activation_token  = User.new_token
+          self.activation_digest = User.digest(activation_token)
+        end
 
 
 end
